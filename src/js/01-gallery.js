d90e2e246e4aad14c18593aef5a2aba93b1fa2bb
Helen Biliak <lbelka7863@gmail.com>
2023-03-01 19:16:27 +0200

    install

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -8,7 +8,7 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 
-// Change code below this line
+// Change code below this line 
 
 const galleryContainer = document.querySelector('.gallery');
 const itemsMarkup = createGalleryItemsMarkup(galleryItems);
